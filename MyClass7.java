1a6e69705111406032dc3f9cd823f434365e6d18
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-10-30 17:50:35 +0000

    P5 Commit

---
+++ MyClass7.java
@@ -0,0 +1,12 @@
+package mypackage7;
+
+
+
+public class MyClass7
+{
+    public static void main(String[] args)
+
+    {
+        System.out.println("What are the Software Testing Techniques?");
+    }
+}
